6ed6976e7732b94cb7aa058b3ad7562683679cb2
nagitausu <nagitaosu.radio@gmail.com>
2019-11-29 17:11:42 +0900

    Fix codeforces color bug

--- plot_rate_distribution.py
+++ plot_rate_distribution.py
@@ -9,22 +9,22 @@ MAX_RATE = 3800
 
 rank = pd.read_csv(sys.argv[1])
 
-ac_color_list = ["#808080", 
-                 "#804000", 
-                 "#008000", 
-                 "#00C0C0", 
-                 "#0000FF", 
-                 "#C0C000", 
-                 "#FF9000", 
+ac_color_list = ["#808080",
+                 "#804000",
+                 "#008000",
+                 "#00C0C0",
+                 "#0000FF",
+                 "#C0C000",
+                 "#FF9000",
                  "#FF0000"]
 
-cf_color_list = ["#808080", 
-                 "#008000", 
-                 "#03a89e", 
-                 "#0000FF", 
-                 "#aa00aa", 
-                 "#FF8C00", 
-                 "#FF8C00", 
+cf_color_list = ["#808080",
+                 "#008000",
+                 "#03a89e",
+                 "#0000FF",
+                 "#aa00aa",
+                 "#FF8C00",
+                 "#FF8C00",
                  "#FF0000",
                  "#000000"]
 cf_color_list += ["#FF0000"] * 10
@@ -48,13 +48,13 @@ for i, p in enumerate(patches):
             color = cf_color_list[2]
         elif rate < 1900:
             color = cf_color_list[3]
-        elif rate < 2200:
+        elif rate < 2100:
             color = cf_color_list[4]
-        elif rate < 2300:
+        elif rate < 2200:
             color = cf_color_list[5]
         elif rate < 2400:
             color = cf_color_list[6]
-        elif rate < 2900:
+        elif rate < 3000:
             color = cf_color_list[7]
         elif rate < 5000:
             color = cf_color_list[8]
@@ -66,7 +66,7 @@ plt.xlim(0, MAX_RATE)
 plt.ylim(0, 2400)
 plt.xlabel("Rating", size=12)
 plt.ylabel("Number of contestants", size=12)
-plt.title("Codeforces Rating Distribution (match >= 5)")
-plt.text(3300, 2410, "2019/11/08", size=12)
+plt.title("Codeforces Rating Distribution (active user, number of participations >= 5)")
+plt.text(3370, 2430, "2019/11/08", size=12)
 plt.tight_layout(True)
 plt.show()
